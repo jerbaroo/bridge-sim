19461eabee45978b27a23367289ad9505caf5a59
Jeremy Barisch Rooney <barischrooneyj@gmail.com>
2019-06-02 11:48:05 +0200

    Show histogram of loading weights

--- normal_load.py
+++ normal_load.py
@@ -1,21 +1,29 @@
+import os
+from functools import reduce
+
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 A16_RAW_DATA = "a16-data/A16.dat"
 A16_CSV_DATA = "a16-data/a16.csv"
 
-col_names = [
+COL_NAMES = [
     "month", "day", "year", "hour", "min", "sec", "number", "lane", "type",
     "speed", "length", "total_weight", "weight_per_axle", "axle_distance"
 ]
 
 
-def convert_to_pandas_csv(in_filepath, out_filepath):
+def raw_to_df_csv(in_filepath, out_filepath, columns=COL_NAMES,
+                  max_rows=10000):
     """Convert the raw A16 data to a csv written by Pandas."""
     with open(in_filepath) as f:
         rows = f.readlines()
-    df = pd.DataFrame(columns=col_names)
-    for i, row in enumerate(rows[:2]):
+    df = pd.DataFrame(columns=columns)
+    part_filepath = lambda num: out_filepath.replace(".", f"{num}.")
+    # After max rows, or last row reached, save to csv.
+    file_number = 0
+    for i, row in enumerate(rows):
         row = row.split()
         values_left = len(row) - 12
         len_axle_weight = (values_left // 2) + 1
@@ -25,19 +33,30 @@ def convert_to_pandas_csv(in_filepath, out_filepath):
         axle_distances = row[
             12 + len_axle_weight:12 + len_axle_weight + len_axle_distance]
         row = simple_cols + [axle_weights] + [axle_distances]
-        df.loc[i] = row
-    print(df.head)
-    df.to_csv(out_filepath)
-
-
-def read_csv_to_dataframe(filepath):
-    """The A16 csv data at given filepath as a Pandas DataFrame."""
-    df = pd.read_csv(filepath)
+        df.loc[i % max_rows] = row
+        if i != 0 and i % 999 == 0:
+            print(f"{i + 1} rows converted")
+        if len(df) == max_rows or i == len(rows) - 1:
+            df.to_csv(part_filepath(num))
+            print(f"Saved {len(df)} rows to {part_filepath(num)}")
+            df = pd.DataFrame(columns=columns)
+            file_number += 1
+    # Merge all saved csv's into a single DataFrame.
+    df_merged = pd.DataFrame(columns=columns)
+    for num in range(file_number):
+        df = pd.read_csv(part_filepath)
+        df_merged = df_merged.append(df, sort=False, ignore_index=True)
+        print(f"Read file {part_filepath}")
     df.set_index("number")
-    return filepath
+    df_merged.to_csv(out_filepath)
+    # Delete temporary files.
+    for num in range(file_number):
+        os.remove(part_filepath(num))
 
 
 if __name__ == "__main__":
-    convert_to_pandas_csv(A16_RAW_DATA, A16_CSV_DATA)
-    df = read_csv_to_dataframe(A16_CSV_DATA)
+    # raw_to_df_csv(A16_RAW_DATA, A16_CSV_DATA)
+    df = pd.read_csv(A16_CSV_DATA, index_col=0)
     print(df.head)
+    plt.hist(df["total_weight"])
+    plt.show()
