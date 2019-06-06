e8bc7580a5b9f8ac5d546ca5f9c19e6594280abb
Jeremy Barisch Rooney <barischrooneyj@gmail.com>
2019-06-06 11:13:03 +0200

    Plot influence lines

--- code/influence_line.py
+++ code/influence_line.py
@@ -8,9 +8,12 @@ from build_opensees_model import build_opensees_model
 from model import Fix, Load, Patch
 from run_opensees_model import run_opensees_model
 
+# Filepath of an influence line matrix based on parameters.
+matrix_filepath = (lambda filepath, num_loads, num_sensors, load, time:
+    f"{filepath}-nl-{num_loads}-ns-{num_sensors}-l-{load}-t-{time}.npy")
 
-def gen_matrix(matrix_path="generated/influence-line-matrix.txt", num_loads=10,
-               num_sensors=100, load=-5e4, time=1):
+
+def gen_matrix(filepath, num_loads, num_sensors, load, time):
     """Generate a matrix of sensor response for each sensor/load position."""
     matrix = np.empty(shape=(num_loads, num_sensors))
     for i, load_position in enumerate(np.linspace(0, 1, num_loads)):
@@ -21,10 +24,25 @@ def gen_matrix(matrix_path="generated/influence-line-matrix.txt", num_loads=10,
         )
         x, y, stress, strain = run_opensees_model()
         matrix[i] = stress[time]
-    np.save(matrix_path, matrix)
+    np.save(
+        matrix_filepath(filepath, num_loads, num_sensors, load, time),
+        matrix)
     plt.imshow(matrix)
+    plt.ylabel("load position")
+    plt.xlabel("sensor position")
     plt.show()
+    return matrix
 
 
 if __name__ == "__main__":
-    gen_matrix()
+    filepath = "generated/il/influence-line-matrix"
+    num_loads = 10
+    num_sensors = 100
+    load = -5e4
+    time = 1
+    matrix = gen_matrix(filepath, num_loads, num_sensors, load, time)
+    matrix = np.load(
+        matrix_filepath(filepath, num_loads, num_sensors, load, time))
+    for i in range(len(matrix)):
+        plt.plot(matrix[i])
+        plt.show()
