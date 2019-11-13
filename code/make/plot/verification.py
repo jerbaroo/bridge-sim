ee91e80889848899b828cc6f296ce52a5e21aed7
Jeremy Barisch-Rooney <barischrooneyj@protonmail.com>
2019-11-13 18:26:18 +0100

    Update convergence plot

--- code/make/plot/verification.py
+++ code/make/plot/verification.py
@@ -8,6 +8,7 @@ from config import Config
 from fem.params import SimParams
 from fem.responses import load_fem_responses
 from fem.run.opensees import OSRunner
+from fem.run.opensees.build.d3 import nodes_by_id
 from model.bridge import Point
 from model.bridge.bridge_705 import bridge_705_3d, bridge_705_config
 from model.load import PointLoad
@@ -49,8 +50,6 @@ def plot_convergence(c: Config, run: bool):
             name=f"Bridge 705 convergence-plot",
             base_mesh_deck_nodes_x=x,
             base_mesh_deck_nodes_z=z,
-            base_mesh_pier_nodes_y=5,
-            base_mesh_pier_nodes_z=5,
             *args,
             **kwargs,
         )
@@ -60,15 +59,18 @@ def plot_convergence(c: Config, run: bool):
 
     if run:
         with open(path + ".txt", "w") as f:
-            pass  # Empty the file.
-        steps = 100
-        xs = np.linspace(2, c.bridge.length * 4, steps)
-        zs = np.linspace(2, c.bridge.width * 4, steps)
+            f.write("x, z, decknodes, time, responses...")
+        steps = 10
+        xs = np.linspace(2, 11, steps)
+        zs = np.linspace(2, 11, steps)
+        # steps = 100
+        # xs = np.linspace(2, c.bridge.length * 4, steps)
+        # zs = np.linspace(2, c.bridge.width * 4, steps)
         for step in range(steps):
             clean_generated(c)
             x, z = int(xs[step]), int(zs[step])
             with open(path + ".txt", "a") as f:
-                f.write(f"\nx = {x}, z = {z}")
+                f.write(f"\n{x}, {z}")
             c = bridge_705_config(bridge_overload)
             start = timer()
             sim_responses = load_fem_responses(
@@ -78,13 +80,14 @@ def plot_convergence(c: Config, run: bool):
                 sim_runner=OSRunner(c),
             )
             response = sim_responses._at(x=point.x, y=point.y, z=point.z)
+            deck_nodes = len([n for n in nodes_by_id.values() if n.deck])
             with open(path + ".txt", "a") as f:
-                f.write(f", time = {timer() - start:.4f}, response = {response}")
+                f.write(f", {deck_nodes}, {timer() - start}, {response}")
 
     with open(path + ".txt") as f:
         results = list(map(
             lambda l: float(l.split()[-1]),
-            filter(lambda l: len(l.split()) > 6, f.readlines())))
+            filter(lambda l: len(l.split()) > 2, f.readlines()[1:])))
     plt.plot(results)
     plt.savefig(path)
     plt.close()
