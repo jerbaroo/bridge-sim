3faaa7b3d1ca3f53d3bc391a18ab35a3a745e5fe
Jeremy Barisch-Rooney <barischrooneyj@protonmail.com>
2019-11-26 13:00:06 +0100

    Started generating sensor table

--- data/verification/convert_sensors.py
+++ data/verification/convert_sensors.py
@@ -14,4 +14,16 @@ for filename in filenames:
     df["x"] = df["x"] / 1000
     df["z"] = df["z"] / 1000 - 16.6
     df["y"] = df["y"] / 1000 - 3.59
-    df.to_csv(filename.replace("-original", ""), float_format="%.3f")
+    # df.to_csv(filename.replace("-original", ""), float_format="%.3f")
+
+    with open(filename.replace("-original", "-org"), "w") as f:
+        f.write("|")
+        f.write("|".join(df.columns))
+        f.write("|\n|")
+        f.write("|".join(map(lambda _: "--", df.columns)))
+        f.write("|\n")
+        for i, row in df.iterrows():
+            print(row)
+            f.write("|")
+            f.write("|".join(map(lambda c: str(row[c]), df.columns)))
+            f.write("|\n")
