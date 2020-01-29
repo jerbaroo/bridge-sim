ea56431b991b293c52452b2a884f631eb78e2702
Jeremy Barisch-Rooney <barischrooneyj@protonmail.com>
2020-01-29 14:34:38 +0100

    Test top lane

--- code/model/test_load.py
+++ code/model/test_load.py
@@ -1,13 +1,32 @@
+from copy import deepcopy
+import numpy as np
+
 from classify.vehicle import wagen1
 from model.bridge.bridge_705 import bridge_705_3d, bridge_705_config
 from model.load import MvVehicle, Vehicle
 
 c = bridge_705_config(bridge_705_3d)
 
+wagen1_top_lane = deepcopy(wagen1)
+wagen1_top_lane.lane = 1
+assert wagen1.lane != wagen1_top_lane.lane
+assert wagen1.init_x_frac == 0
 
-def test_mv_vehicle_leaves_bridge():
-    assert wagen1.init_x_frac == 0
+
+def test_mv_vehicle_time_leaving_bridge():
+    # Bottom lane.
     assert c.bridge.length / wagen1.mps == wagen1.time_leaving_bridge(c.bridge)
+    # Top lane.
+    assert c.bridge.length / wagen1_top_lane.mps == wagen1_top_lane.time_leaving_bridge(c.bridge)
+
+
+def test_mv_vehicle_time_left_bridge():
+    # Bottom lane.
+    time_to_leave = wagen1.time_left_bridge(c.bridge) - wagen1.time_leaving_bridge(c.bridge)
+    assert np.isclose(wagen1.length / wagen1.mps, time_to_leave)
+    # Top lane.
+    time_to_leave = wagen1_top_lane.time_left_bridge(c.bridge) - wagen1_top_lane.time_leaving_bridge(c.bridge)
+    assert np.isclose(wagen1_top_lane.length / wagen1_top_lane.mps, time_to_leave)
 
 
 # def test_mv_vehicle_to_point_loads():
