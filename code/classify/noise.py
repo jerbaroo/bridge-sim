4e9ad45955beec533ccf390ea8a03281a9117695
Jeremy Barisch-Rooney <barischrooneyj@protonmail.com>
2020-01-31 14:09:44 +0100

    Add noise to truck 1 time series plots

---
+++ code/classify/noise.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+def add_displa_noise(a, std: float=0.0012 / 1000, mean: float=0):
+    """Add noise to a time series of displacements in meters.
+
+    Default standard deviation of noise is 0.0012mm with a mean of 0.
+
+    """
+    return np.array(a) + np.random.normal(mean, std, len(a))
