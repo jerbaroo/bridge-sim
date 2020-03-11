22e09a218181e3db828ebfc952d209866aa07fa4
Jeremy Barisch-Rooney <barischrooneyj@protonmail.com>
2020-03-11 13:16:21 +0100

    Update ntoebooks

--- code/classify/temperature.py
+++ code/classify/temperature.py
@@ -187,13 +187,13 @@ def effect(
     )
     print_d(D, f"unit uniform and linear = {unit_uniforms} {unit_linears}")
     # Determine temperature gradient throughout the bridge.
-    if temps_bt is not None:
-        temps_bottom, temps_top = temps_bt
-        temps_bottom, temps_top = np.array(temps_bottom), np.array(temps_top)
-    else:
+    if temps_bt is None:
         temps_bottom, temps_top = temps_bottom_top(
             c=c, temps=temps, len_per_hour=len_per_hour
         )
+    else:
+        temps_bottom, temps_top = temps_bt
+        temps_bottom, temps_top = np.array(temps_bottom), np.array(temps_top)
     temps_half = (temps_bottom + temps_top) / 2
     temps_linear = temps_top - temps_bottom
     temps_uniform = temps_half - c.bridge.ref_temp_c
@@ -229,40 +229,47 @@ def resize(temps, tmin=-5, tmax=35):
     )(temps)
 
 
-def get_effect(
+def apply_effect(
     c: Config,
-    response_type: ResponseType,
     points: List[Point],
     responses: List[List[float]],
-    temps: List[float],
+    effect: List[List[float]],
     speed_up: int = 1,
     repeat_responses: bool = False,
 ) -> List[float]:
-    """Temperature effect corresponding to time series at a number of points."""
+    """Time series of effect due to temperature at given points.
+
+    Returns: a NumPy array of shape the same as given responses. The effect due
+        to temperature is interpolated across the date range of the given
+        responses, this is calculated under the assumption that temperature
+        effect is given at one data point per minute and that the sensor
+        responses are given at a rate of 'c.sensor_hz'.
+
+    """
     assert len(responses) == len(points)
     # Convert the temperature data into temperature effect at each point.
-    effect_ = effect(c=c, response_type=response_type, points=points, temps=temps)
-    assert len(effect_) == len(points)
+    # effect_ = effect(c=c, response_type=response_type, points=points, temps=temps)
+    assert len(effect) == len(points)
     # A temperature sample is available per minute. Here we calculate the
     # number of responses between each pair of recorded temperatures and the
     # number of temperature samples required for the given responses.
     len_per_min = get_len_per_min(c=c, speed_up=speed_up)
-    print(f"len per min = {len_per_min}")
+    print_i(f"Length per minute = {len_per_min}, speed_up = {speed_up}")
     num_temps_req = math.ceil(len(responses[0]) / len_per_min) + 1
-    if num_temps_req > len(effect_[0]):
+    if num_temps_req > len(effect[0]):
         raise ValueError(
-            f"Not enough temperatures ({len(effect_[0])}) for data"
+            f"Not enough temperatures ({len(effect[0])}) for data"
             f" (requires {num_temps_req})"
         )
     # If additional temperature data is available, then use it if requested and
     # repeat the given responses. Here we calculate length, in terms of the
     # sample frequency, recall that temperature is sampled every minute.
-    avail_len = (len(effect_[0]) - 1) * len_per_min
+    avail_len = (len(effect[0]) - 1) * len_per_min
     if repeat_responses and (avail_len > len(responses[0])):
         print_i(
             f"Increasing length of responses from {len(responses[0])} to {avail_len}"
         )
-        num_temps_req = len(effect_[0])
+        num_temps_req = len(effect[0])
         new_responses = np.empty((len(responses), avail_len))
         for i in range(len(responses)):
             for j in range(math.ceil(avail_len / len(responses[0]))):
@@ -271,20 +278,20 @@ def get_effect(
                 new_responses[i][start:end] = responses[i][: end - start]
         responses = new_responses
     # Fill in the responses array with the temperature effect.
-    result = np.zeros((len(responses), len(responses[0])))
-    for i in range(len(responses)):
+    result = np.zeros((len(points), len(responses[0])))
+    for i in range(len(points)):
         for j in range(num_temps_req - 1):
             start = j * len_per_min
             end = min(len(result[i]), start + len_per_min)
             print_d(D, f"start = {start}")
             print_d(D, f"end = {end}")
             print_d(D, f"end - start = {end - start}")
-            print_d(D, f"temp_start, temp_end = {temps[j]}, {temps[j + 1]}")
+            # print_d(D, f"temp_start, temp_end = {temps[j]}, {temps[j + 1]}")
             print_d(
-                D, f"effect_start, effect_end = {effect_[i][j]}, {effect_[i][j + 1]}"
+                D, f"effect_start, effect_end = {effect[i][j]}, {effect[i][j + 1]}"
             )
             result[i][start:end] = np.linspace(
-                effect_[i][j], effect_[i][j + 1], end - start
+                effect[i][j], effect[i][j + 1], end - start
             )
     if repeat_responses:
         return responses, result
