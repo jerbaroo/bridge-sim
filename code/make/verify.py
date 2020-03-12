b9d90cd9c73e8f914ac0ea91c4aa1626b05f61f5
Jeremy Barisch-Rooney <barischrooneyj@protonmail.com>
2020-03-12 15:02:47 +0100

    Plot'

--- code/make/verify.py
+++ code/make/verify.py
@@ -389,11 +389,11 @@ def wagen_1_contour_plot(c: Config, x: int, response_type: ResponseType):
     plt.landscape()
     plt.subplot(2, 1, 1)
     top_view_bridge(bridge=c.bridge, compass=False, abutments=True, piers=True)
-    plot_contour_deck(c=c, responses=healthy_responses, ploads=loads if LOADS else [], scatter=True)
+    plot_contour_deck(c=c, responses=healthy_responses, ploads=loads if LOADS else [], scatter=True, norm=norm)
     plt.title("Truck 1 on healthy bridge")
     plt.subplot(2, 1, 2)
     top_view_bridge(bridge=c.bridge, compass=False, abutments=True, piers=True)
-    plot_contour_deck(c=c, responses=crack_responses, ploads=loads if LOADS else [], scatter=True)
+    plot_contour_deck(c=c, responses=crack_responses, ploads=loads if LOADS else [], scatter=True, norm=norm)
     plt.title("Truck 1 on cracked bridge")
     plt.tight_layout()
     plt.savefig(
