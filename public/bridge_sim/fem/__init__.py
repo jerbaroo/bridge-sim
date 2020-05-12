78dcd87bec24aa527a9f33152f2d4e961699aacc
Jeremy Barisch-Rooney <barischrooneyj@protonmail.com>
2020-05-12 13:56:06 +0100

    Consistent naming update

--- public/bridge_sim/fem/__init__.py
+++ public/bridge_sim/fem/__init__.py
@@ -1,16 +1,17 @@
 from typing import List, Optional
 
-from bridge_sim.model import PointLoad, PierSettlement
+from bridge_sim.model import PierSettlement
 from lib.config import Config as LibConfig
 from lib.fem.params import SimParams as LibSimParams
 from lib.fem.responses import load_fem_responses as lib_load_fem_responses
+from lib.model.load import PointLoad as LibPointLoad
 from lib.model.response import ResponseType as LibResponseType
 
 
 def responses(
     config: LibConfig,
     response_type: LibResponseType,
-    point_loads: List[PointLoad] = [],
+    point_loads: List[LibPointLoad] = [],
     pier_settle: Optional[PierSettlement] = None,
 ):
     return lib_load_fem_responses(
