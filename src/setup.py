2b4658dda86713c25740cd0531aecdf26720c32c
Jeremy Barisch-Rooney <barischrooneyj@protonmail.com>
2020-05-13 22:52:06 +0100

    Add simple setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,22 +1,3 @@
-import pypandoc
-import setuptools
+from distutils.core import setup
 
-long_description = pypandoc.convert_file("../README.org", "md")
-
-setuptools.setup(
-    name="bridge-sim-barischrooneyj",  # Replace with your own username
-    version="0.0.5",
-    author="Jeremy Barisch-Rooney",
-    author_email="barischrooneyj@protonmail.com",
-    description="A Python library for concrete slab bridge simulation.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/barischrooneyj/bridge-sim",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.7",
-)
+setup(name="bridge_sim", version="0.1")
