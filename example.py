8026624f5b24618b43302d0dabf0b63697552262
Jeremy Barisch-Rooney <barischrooneyj@protonmail.com>
2020-05-15 20:09:36 +0100

    Add custom bridge

--- example.py
+++ example.py
@@ -1,15 +1,15 @@
 # Example 1.
 
-import matplotlib.pyplot as plt
-from bridge_sim import bridges, configs, fem, model, plot
+# import matplotlib.pyplot as plt
+# from bridge_sim import bridges, configs, fem, model, plot
 
-config = configs.opensees_default(bridges.bridge_example)
-point_loads = [model.PointLoad(config, x=5, z=0, load=100)]
-responses = fem.responses(config, model.RT.YTranslation, point_loads)
-plot.contour_responses(config, responses, point_loads)
-plot.top_view_bridge(config, piers=True)
-plt.tight_layout()
-plt.show()
+# config = configs.opensees_default(bridges.bridge_example)
+# point_loads = [model.PointLoad(config, x=5, z=0, load=100)]
+# responses = fem.responses(config, model.RT.YTranslation, point_loads)
+# plot.contour_responses(config, responses, point_loads)
+# plot.top_view_bridge(config, piers=True)
+# plt.tight_layout()
+# plt.show()
 
 # Example 2.
 
@@ -64,3 +64,50 @@ plt.show()
 
 # plt.tight_layout()
 # plt.show()
+
+# Example 5.
+
+import matplotlib.pyplot as plt
+from bridge_sim import bridges, configs, fem, model, plot
+from bridge_sim.bridges import Bridge, Lane, MaterialDeck, MaterialSupport, Support
+
+
+def new_bridge():
+    return Bridge(
+        name="example",   # Name used to identify saved/loaded data.
+        length=40,  # Length of this bridge.
+        width=3,  # Width of this bridge.
+        supports=[
+            Support(
+                x=20,  # X position of center of the support.
+                z=0,  # Z position of center of the support.
+                length=2,  # Length between support columns (X direction).
+                height=2,  # Height from top to bottom of support.
+                width_top=2,  # Width of support column at top (Z direction).
+                width_bottom=1,  # Width of support column at bottom (Z direction).
+                materials=[  # List of materials for the support columns.
+                    MaterialSupport(
+                        density=0.7,
+                        thickness=0.7,
+                        youngs=40000,
+                        poissons=0.2,
+                        start_frac_len=0,
+                    )
+                ],
+                fix_z_translation=True,
+                fix_x_translation=True,
+            )
+        ],
+        # List of materials for the bridge deck.
+        materials=[MaterialDeck(thickness=0.7, youngs=40000, poissons=0.2,)],
+        # List of lanes where traffic can drive on the bridge.
+        lanes=[Lane(-1, 1, True)],
+    )
+
+config = configs.opensees_default(new_bridge)
+point_loads = [model.PointLoad(config, x=18, z=0, load=100)]
+responses = fem.responses(config, model.RT.YTranslation, point_loads)
+plot.contour_responses(config, responses, point_loads)
+plot.top_view_bridge(config, piers=True)
+plt.tight_layout()
+plt.show()
