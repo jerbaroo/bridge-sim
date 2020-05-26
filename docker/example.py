35b83c05da8aae924feccd3c0319ab0766fafec5
Jeremy Barisch-Rooney <barischrooneyj@protonmail.com>
2020-05-26 17:54:52 +0100

    Settlement of multiple piers

--- docker/example.py
+++ docker/example.py
@@ -1,13 +1,11 @@
 from pathlib import Path
 
-import bridge_sim.sim
-import bridge_sim.sim.responses
 import matplotlib.pyplot as plt
-from bridge_sim import bridges, configs, fem, model, plot
+from bridge_sim import bridges, configs, model, plot, sim
 
 config = configs.opensees_default(bridges.bridge_narrow)
 point_loads = [model.PointLoad(x=5, z=0, load=100)]
-responses = bridge_sim.sim.responses.responses(config, model.RT.YTrans, point_loads)
+responses = sim.responses.load(config, model.RT.YTrans, point_loads)
 plot.contour_responses(config, responses, point_loads)
 plot.top_view_bridge(config, piers=True)
 plt.tight_layout()
