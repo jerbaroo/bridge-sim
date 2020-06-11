4f09978c4154e46c0f1c8794e590569d21b7c592
Jeremy Barisch-Rooney <barischrooneyj@protonmail.com>
2020-06-11 17:14:43 +0100

    Fix truck 1 plot

--- src/lib/make/plot/vehicle.py
+++ src/lib/make/plot/vehicle.py
@@ -1,35 +1,35 @@
 """Plots for individual vehicles."""
-import matplotlib.image as mpimg
 
-from bridge_sim.model import Config
-from bridge_sim.vehicles import truck1
+import matplotlib.pyplot as plt
+
+from bridge_sim.model import Config, Vehicle
 from lib.plot.vehicle import topview_vehicle
-from lib.plot import plt
 
 
-def wagen1_plot(c: Config):
-    """Plot of wagen1 compared to given specification."""
+def comparison(c: Config, v1: Vehicle, v2: Vehicle):
+    """Plot of a vehicle with wheel print compared to with point-loads."""
     plt.landscape()
 
     wheel_print = (0.31, 0.25)
     wheel_prints = []
-    for w_i in range(len(truck1.axle_distances) + 1):
+    for w_i in range(len(v1.axle_distances) + 1):
         if w_i in [1, 2]:
             wheel_prints.append([wheel_print, wheel_print])
         else:
             wheel_prints.append([wheel_print])
 
     plt.subplot(1, 2, 1)
-    xlim, ylim = topview_vehicle(truck1, wheel_prints=wheel_prints)
+    xlim, ylim = topview_vehicle(v1, wheel_prints=wheel_prints)
     plt.title("Truck 1 specification")
     plt.xlabel("Width (m)")
     plt.ylabel("Length (m)")
 
     plt.subplot(1, 2, 2)
-    topview_vehicle(truck1, xlim=xlim, ylim=ylim)
+    topview_vehicle(v2, xlim=xlim, ylim=ylim)
     plt.title("Truck 1 in simulation")
     plt.xlabel("Width (m)")
     plt.ylabel("Length (m)")
 
     plt.savefig(c.get_image_path("vehicles", "wagen-1", bridge=False) + ".pdf")
     plt.close()
+
