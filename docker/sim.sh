bfad047b3dd260b6d5eb7beb9fd0d06651a778c8
Jeremy Barisch-Rooney <barischrooneyj@protonmail.com>
2020-06-11 09:54:23 +0100

    Add singularity scripts

--- docker/sim.sh
+++ docker/sim.sh
@@ -1,2 +1,24 @@
 cd /bridge-sim
-./scripts/cli.sh --parallel 14 --shorten-paths --save-to $HOME/gen-data/inflines --msl 0.5 validate inflines --strain-sensors O
+
+# Verification.
+./scripts/cli.sh --two-materials --save-to $HOME/gen-data --msl 0.5 verify point-loads
+./scripts/cli.sh --two-materials --save-to $HOME/gen-data --msl 0.5 verify pier-settlement
+./scripts/cli.sh --two-materials --save-to $HOME/gen-data --msl 0.5 verify temp-loads
+./scripts/cli.sh --save-to $HOME/gen-data --msl 0.5 verify shrinkage
+./scripts/cli.sh --save-to $HOME/gen-data --msl 0.5 verify creep
+./scripts/cli.sh --save-to $HOME/gen-data --msl 0.5 verify temp-effect
+./scripts/cli.sh --save-to $HOME/gen-data --msl 0.5 --parallel 14 verify uls
+./scripts/cli.sh --save-to $HOME/gen-data --msl 0.5 --parallel 14 verify ulm
+./scripts/cli.sh --save-to $HOME/gen-data --msl 0.5 verify strain
+./scripts/cli.sh --save-to $HOME/gen-data --msl 0.5 verify traffic
+./scripts/cli.sh --save-to $HOME/gen-data --msl 0.5 verify responses
+
+# Validation.
+./scripts/cli.sh --save-to $HOME/gen-data --msl 0.5 --parallel 14 validate dynamic
+./scripts/cli.sh --save-to $HOME/gen-data --msl 0.5 --parallel 14 validate r2
+# ./scripts/cli.sh --parallel 14 --shorten-paths --save-to $HOME/gen-data --msl 0.5 validate inflines --strain-sensors O
+# ./scripts/cli.sh --parallel 14 --shorten-paths --save-to $HOME/gen-data --msl 0.5 validate inflines --strain-sensors T
+
+# Thesis.
+./scripts/cli.sh --save-to $HOME/gen-data --msl 0.5 thesis temp-profile
+./scripts/cli.sh --save-to $HOME/gen-data --msl 0.5 thesis temp-contour
