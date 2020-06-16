cac1cde692a17a57521757abacbe3fde7ca0d309
Jeremy Barisch-Rooney <barischrooneyj@protonmail.com>
2020-06-16 14:56:56 +0100

    Fix creep test

--- tests/test_creep.py
+++ tests/test_creep.py
@@ -2,12 +2,13 @@
 
 import numpy as np
 
-from bridge_sim import creep, shrinkage
+from bridge_sim import configs, creep, shrinkage
 from bridge_sim.util import convert_times
 
 
 def test_creep_non_negative():
+    config, _ = configs.test_config(msl=10)
     days = np.arange(365)
     seconds = convert_times(f="day", t="second", times=days)
-    strain = creep.creep(shrinkage.CementClass.Normal, h_0=250, times=seconds)
+    strain = creep.creep_coeff(config, shrinkage.CementClass.Normal, times=seconds, x=51)
     assert strain[0] >= 0
