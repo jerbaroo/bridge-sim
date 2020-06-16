a0b07fea4fa389b8ff43960f71b59c459aaa3ac2
Jeremy Barisch-Rooney <barischrooneyj@protonmail.com>
2020-06-16 20:44:56 +0100

    Add asphalt test

--- tests/test_build.py
+++ tests/test_build.py
@@ -1,11 +1,12 @@
 import numpy as np
 
-from bridge_sim import bridges
+from bridge_sim import bridges, configs
 from bridge_sim.sim.build import get_bridge_shells
 
 
 def test_material_and_area_and_mass():
-    bridge_705 = bridges.bridge_705(msl=10)()
+    config, _ = configs.test_config(msl=10)
+    bridge_705 = config.bridge
     x, z = 0, -16.6
     material = bridge_705.deck_section_at(x=x, z=z)
     thickness = material.thickness
@@ -20,4 +21,26 @@ def test_material_and_area_and_mass():
     length, width = shell.length(), shell.width()
     assert np.isclose(shell.area(), length * width)
     # Assert mass is correct.
-    assert np.isclose(shell.mass(), thickness * density * length * width)
+    assert np.isclose(shell.mass(config), thickness * density * length * width)
+
+
+def test_asphalt_mass():
+    config, _ = configs.test_config(msl=2)
+    deck_shells, _ = get_bridge_shells(config.bridge)
+    # Not on lane.
+    shell = deck_shells[0]
+    config.self_weight_asphalt = True
+    mass_with_asphalt = shell.mass(config)
+    config.self_weight_asphalt = False
+    mass_without_asphalt = shell.mass(config)
+    assert mass_with_asphalt == mass_without_asphalt
+    # On lane.
+    lane = config.bridge.lanes[0]
+    for shell in deck_shells:
+        if lane.z_min <= shell.center().z <= lane.z_max:
+            config.self_weight_asphalt = True
+            mass_with_asphalt = shell.mass(config)
+            config.self_weight_asphalt = False
+            mass_without_asphalt = shell.mass(config)
+        continue
+    assert mass_with_asphalt > mass_without_asphalt
