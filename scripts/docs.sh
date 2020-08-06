e53a204be32f6c857be41b8f396b67ead9bd64c3
Jeremy Barisch-Rooney <barischrooneyj@protonmail.com>
2020-08-06 18:41:33 +0100

    Poetry (#176)
    Pipenv -> Poetry, v0.0.8

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -1,5 +1,5 @@
 rm -rf docs/
-pipenv run pdoc --html --output-dir build bridge_sim
+poetry run pdoc --html --output-dir build bridge_sim
 mv build/bridge_sim docs/
 rm -rf build/
-pipenv run python scripts/docs-update.py
+poetry run python scripts/docs-update.py
