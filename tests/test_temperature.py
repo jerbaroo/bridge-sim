b53bde622675ecc8ffa2ed1764d32c0f7356a8fc
barischrooneyj <barischrooneyj@protonmail.com>
2020-08-11 12:23:55 +0100

    Update tests for new data

--- tests/test_temperature.py
+++ tests/test_temperature.py
@@ -7,14 +7,14 @@ from bridge_sim import temperature
 
 def test_resize():
     tmin, tmax = -20, 200
-    weather = temperature.load("holly-springs")
+    weather = temperature.load("holly-springs-19")
     weather["temp"] = temperature.resize(weather["temp"], tmin=tmin, tmax=tmax)
     assert max(weather["temp"]) == tmax
     assert min(weather["temp"]) == tmin
 
 
 def test_from_to_mins():
-    weather = temperature.load("holly-springs")
+    weather = temperature.load("holly-springs-19")
     from_ = datetime.fromisoformat(f"2019-01-01T00:00")
     to = datetime.fromisoformat(f"2019-01-01T00:59")
     weather = temperature.from_to_mins(weather, from_, to)
