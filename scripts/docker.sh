f866847ab7d833f9b019e3701d752c26abe2e267
barischrooneyj <barischrooneyj@protonmail.com>
2020-08-19 19:13:28 +0100

    Remove uneeded file

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,6 +1,6 @@
 CAT << EOF
 docker login --username=barischrooneyj
-docker build -f docker/Dockerfile
+docker build -f docker/Dockerfile .
 docker tag Y barischrooneyj/bridge-sim:version
 docker push barischrooneyj/bridge-sim:version
 EOF
